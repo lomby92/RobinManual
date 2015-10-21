b9968447877c92569e6cb6da83f07c574140e7d9
Lorenzo <lorenzo.millucci@outlook.com>
2015-10-21 17:24:17 +0200

    Pulizia

--- src/Motor.java
+++ src/Motor.java
@@ -3,8 +3,8 @@
  */
 public class Motor extends Thread {
 
-    private int actualVel1 = 0;
-    private int actualVel2 = 0;
+    private int actualVelocity1 = 0;
+    private int actualVelocity2 = 0;
 
     private final int[] SOGLIA_UP = {35, 65, 85, 100};
     private final int[] SOGLIA_DOWN = {40, 65, 100};
@@ -29,10 +29,10 @@ public class Motor extends Thread {
             int desVel1 = desiredVel[0];
             int desVel2 = desiredVel[1];
 
-            actualVel1 = getLimitedVel(desVel1, actualVel1);
-            actualVel2 = getLimitedVel(desVel2, actualVel2);
+            actualVelocity1 = getLimitedVel(desVel1, actualVelocity1);
+            actualVelocity2 = getLimitedVel(desVel2, actualVelocity2);
 
-            System.out.println(actualVel1 + " - " + actualVel2);
+            System.out.println(actualVelocity1 + " - " + actualVelocity2);
 
             if(timer > 5){
                 int[] vel = {0, 0};
@@ -51,18 +51,18 @@ public class Motor extends Thread {
         }
     }
 
-    private int getLimitedVel(int des, int actual){
-        if(actual < des){
+    private int getLimitedVel(int desired, int actual){
+        if(actual < desired){
             //accelero
-            actual = actual +30;
+            actual = actual + 30;
             actual = Math.min(actual, 100);
-            actual = Math.min(actual, des);
+            actual = Math.min(actual, desired);
 
         }else{
             //rallento
-            actual = actual -30;
+            actual = actual - 30;
             actual = Math.max(-100, actual);
-            actual = Math.max(actual, des);
+            actual = Math.max(actual, desired);
         }
         return actual;
     }
