2979f2fb90cdc35ed34b2b618702784ce7ae2c81
Lorenzo <lorenzo.millucci@outlook.com>
2015-10-21 17:22:33 +0200

    Aggiunto controllo validità delle velocità massime e minime

--- src/Status.java
+++ src/Status.java
@@ -26,6 +26,15 @@ public class Status {
     }
 
     public synchronized void setVel(int[] vel){
+
+        //Controllo che la velocità non sia minore della minima
+        vel[0] = Math.max(vel[0], -100);
+        vel[1] = Math.max(vel[1], -100);
+        //Controllo che la velocità non sia maggiore della massima
+        vel[0] = Math.min(vel[0], 100);
+        vel[1] = Math.min(vel[1], 100);
+
+
         this.targetMotor1 = vel[0];
         this.targetMotor2 = vel[1];
         isFresh = true;
