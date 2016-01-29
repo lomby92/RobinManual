f485b81ebf98569c71ab1f99e80f9f5ec2ce3f84
DarkAvenger36 <lorenzo.millucci@outlook.com>
2016-01-29 11:47:27 +0100

    Implementato il pattern observer e aggiunta la variabile di stato per il rullo

--- src/server/Status.java
+++ src/server/Status.java
@@ -3,18 +3,22 @@ package server;
 import java.util.Observable;
 
 /**
- * Created by lmillucci on 20/10/15.
+ * Questa classe rappresenta lo stato del robot.
+ * Lo stato e' costituito dalle velocita' dei motori (int) e dall'abilitazione del motore del rullo (boolean)
+ *
+ * @Author Lorenzo Millucci
+ * @Since 29/01/16
  */
 public class Status extends Observable{
 
     private int targetMotor1, targetMotor2;
-    private boolean isFresh;
     private static Status status = new Status();
+    private boolean rollEnabled;
 
     private  Status(){
         targetMotor1 = 0;
         targetMotor2 = 0;
-        isFresh = true;
+        rollEnabled = false;
     }
 
     public static Status getInstance(){
@@ -25,31 +29,28 @@ public class Status extends Observable{
         int[] tmp = new int[2];
         tmp[0] = targetMotor1;
         tmp[1] = targetMotor2;
-        isFresh = false;
         return tmp;
     }
 
-    public synchronized void setVel(int[] vel){
+    public synchronized void setStatus(int motor1, int motor2, boolean rollEnabled){
 
-        //Controllo che la velocita non sia minore della minima
-        vel[0] = Math.max(vel[0], -100);
-        vel[1] = Math.max(vel[1], -100);
-        //Controllo che la velocita non sia maggiore della massima
-        vel[0] = Math.min(vel[0], 100);
-        vel[1] = Math.min(vel[1], 100);
+        //Controllo che la velocita' non sia minore della minima
+        motor1 = Math.max(motor1, -100);
+        motor2 = Math.max(motor2, -100);
+        //Controllo che la velocita' non sia maggiore della massima
+        motor1 = Math.min(motor1, 100);
+        motor2 = Math.min(motor2, 100);
 
 
-        this.targetMotor1 = vel[0];
-        this.targetMotor2 = vel[1];
-        isFresh = true;
+        this.targetMotor1 = motor1;
+        this.targetMotor2 = motor2;
+        this.rollEnabled = rollEnabled;
+
 
         //Ho appena aggiornato lo stato e quindi lo notifico agli osservatori
         setChanged();
         notifyObservers();
     }
 
-    public synchronized boolean isFresh(){
-        return this.isFresh;
-    }
 
 }
