7b7e0ce84b59a41c967c9992723e65e7c477b8f4
DarkAvenger36 <lorenzo.millucci@outlook.com>
2016-01-29 11:45:34 +0100

    Aggiunti i comandi per impostare la tipologia di filtro del motore e per impostare il motore come osservatore dello stato

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -1,4 +1,6 @@
-import java.net.Socket;
+package server;
+
+import server.filter.LowPassFilter;
 
 /**
  * Created by lmillucci on 20/10/15.
@@ -8,33 +10,42 @@ public class Controller {
     public static  void main(String[] args){
         Connection server = new Connection();
         Motor motor = new Motor();
+
+        //imposto i parametri necessari al funzionamento del motore
+        motor.setFilterStrategy(new LowPassFilter());
+        Status.getInstance().addObserver(motor);
+
         motor.start();
 
         String msg = "";
         while (true){
             server.waitConnection();
             msg = "";
-            while(msg!=null && !msg.equals("end")) {
+            while(msg!=null && !msg.toLowerCase().equals("end")) {
                 msg = server.getMessage();
-                //System.out.println(msg);
-                if( msg!= null){
+                System.out.println(msg);
+                if( msg != null){
                     String tmp[]  = msg.split(";");
 
-                    if(tmp.length == 2){
+                    if(tmp.length == 3){
                         try {
                             int vel1 = Integer.parseInt(tmp[0]);
                             int vel2 = Integer.parseInt(tmp[1]);
-                            int[] v = {vel1, vel2};
-                            Status.getInstance().setVel(v);
+                            boolean roll = Integer.parseInt(tmp[2]) == 1 ? true : false;
+                            Status.getInstance().setStatus(vel1, vel2, roll);
+
                         }catch (NumberFormatException nfe){
                             System.out.println("Errore nel formato dei dati ricevuti");
                             nfe.printStackTrace();
                         }
+                    }else{
+                        server.sendMessage("Inviato messaggio non valido!\r\n");
                     }
 
                 }
 
             }
+            server.sendMessage("Connessione chiusa! \r\n");
             server.disconnect();
         }
     }
