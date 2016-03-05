a4d83be198e616bbe5828eea4057de30683a7a3f
DarkAvenger36 <lorenzo.millucci@outlook.com>
2016-03-05 14:55:09 +0100

    Aggiunto il classpath per Pi4J

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
 THE_BUILDPATH=../build
 PROGRAM_NAME=server.Controller.java
+
 cd src
 
-javac -d "${THE_BUILDPATH}" $PROGRAM_NAME
+javac -classpath .:classes:/opt/pi4j/lib/'*' -d "${THE_BUILDPATH}" $PROGRAM_NAME
 
 if [ $? -eq 0 ]
 then
