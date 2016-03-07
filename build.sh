0762fde325320f442a06ba40f95ae5d4a38801ad
Lorenzo <lorenzo.millucci@outlook.com>
2016-03-07 16:17:45 +0000

    Sistemato lo script di compilazione

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 BUILDPATH=../build
-PROGRAM_NAME=server.Controller.java
+PROGRAM_NAME=server/*.java
 
 cd src
 
 javac -classpath .:classes:/opt/pi4j/lib/'*' -d "${BUILDPATH}" $PROGRAM_NAME
 
-javac -classpath .:classes:/opt/pi4j/lib/'*' -d "${BUILDPATH}" server.filter.*.java
+javac -classpath .:classes:/opt/pi4j/lib/'*' -d "${BUILDPATH}" server/filter/*.java
 
 if [ $? -eq 0 ]
 then
